d8ce055d898270ee48e55992e2d49200a039826e
@Transdev_ <lunasemfronteiras@gmail.com>
2021-05-21 20:34:30 -0300

    Adicionei o resultado da atividade7

---
+++ atividade7.js
@@ -0,0 +1,12 @@
+// 7 - Elabore um algoritmo que receba dois números e determine qual é o maior entre eles, se os números forem iguais, mostre uma mensagem no console "Os números são iguais".
+
+let num1 = 45
+let num2 = 67
+
+if (num1 < num2) {
+    console.log(`${num1} é menor`)
+} else if (num1 > num2) {
+    console.log(`${num2} é menor`)
+} else {
+    console.log (`Os números são iguais`)
+}
