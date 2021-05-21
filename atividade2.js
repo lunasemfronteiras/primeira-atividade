a3c82962856a52045484f88f1de37467554c1acd
@Transdev_ <lunasemfronteiras@gmail.com>
2021-05-21 20:31:52 -0300

    Adicionei o resultado da atividada2

---
+++ atividade2.js
@@ -0,0 +1,9 @@
+// 2 - Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+
+let distancia = 100; //km
+let velocidadeMedia = 80; //km/h
+
+let tempo = distancia / velocidadeMedia; 
+console.log(`O tempo da viagem foi de ${tempo} Horas`)
+
+
