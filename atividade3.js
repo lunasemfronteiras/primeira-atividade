0e90182a10efca42321f3e4d2def4c5878293e7b
@Transdev_ <lunasemfronteiras@gmail.com>
2021-05-21 20:32:15 -0300

    Adicionei o resultado da atividade3

---
+++ atividade3.js
@@ -0,0 +1,9 @@
+// 3 - Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
+
+let valorJantar = 138
+let taxa = valorJantar * (10 / 100)
+
+let valorTotal = valorJantar + taxa
+
+console.log(taxa)
+console.log(valorTotal)
