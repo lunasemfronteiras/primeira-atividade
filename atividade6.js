8d80f6b06f9dccf977e480426e3bb366a80231b5
@Transdev_ <lunasemfronteiras@gmail.com>
2021-05-21 20:33:43 -0300

    Adicionei o resultado da atividade6

---
+++ atividade6.js
@@ -0,0 +1,75 @@
+// 6 - Elabore um algoritmo que receba um número (1-7) e devolva o dia da semana correspondente.
+
+let entrada = 4
+ 
+if (entrada === 1){
+    console.log("É domingo!")
+}
+
+if (entrada ===2){
+    console.log("É segunda!")
+}
+
+if (entrada ===3){
+    console.log("É terça!")
+}
+
+if (entrada ===4){
+    console.log("É quarta!")
+}
+
+if (entrada ===5){
+    console.log("É quinta!")
+}
+
+if (entrada ===6){
+    console.log("É sexta!")
+}
+
+if (entrada ===7){
+    console.log("É sábado!")
+}
+
+//opção2
+
+switch (entrada){
+    case 1:
+        console.log("É domingo!")
+        break
+}
+
+switch (entrada){
+    case 2:
+        console.log("É segunda!")
+        break
+}
+
+switch (entrada){
+    case 3:
+        console.log("É terça!")
+        break
+}
+
+switch (entrada){
+    case 4:
+        console.log("É quarta!")
+        break
+}
+
+switch (entrada){
+    case 5:
+        console.log("É quinta!")
+        break
+}
+
+switch (entrada){
+    case 6:
+        console.log("É sexta!")
+        break
+}
+
+switch (entrada){
+    case 7:
+        console.log("É sábado!")
+        break
+}
