64fd7a4d342db6ae030f40b30550ccb5c2614bed
@Transdev_ <lunasemfronteiras@gmail.com>
2021-05-21 20:33:12 -0300

    Adicionei o resultado da atividade5

---
+++ atividade5.js
@@ -0,0 +1,14 @@
+// 5 - Receba do usuários 3 números e calcule a média entre eles.
+
+//opção1
+let numero1 = 10
+let numero2 = 10
+let numero3 = 10
+
+let media = (numero1 = numero2 = numero3)/3
+console.log(media)
+
+//opção2
+
+let media2 = (10+10+10)/3
+console.log(media2)
