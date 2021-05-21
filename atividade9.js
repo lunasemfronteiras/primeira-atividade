f1c060d0e0bcbf37016364f41d7fb8b5de232458
@Transdev_ <lunasemfronteiras@gmail.com>
2021-05-21 20:35:42 -0300

    Adicionei o resultado da atividade9

---
+++ atividade9.js
@@ -0,0 +1,10 @@
+// 9 - Crie uma função que retorna a palavra (impar/par) de acordo com seu parâmetro.
+
+
+let entrada = 25;
+
+if (entrada % 2 === 0){
+    console.log("par");
+} else {
+    console.log("impar");
+}
