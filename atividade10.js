a77401542a05a6485320ed5e9329a8b822ff77c9
@Transdev_ <lunasemfronteiras@gmail.com>
2021-05-21 20:36:11 -0300

    Adicionei o resultado da atividade10

---
+++ atividade10.js
@@ -0,0 +1,14 @@
+// 10 - Crie uma função que recebe o ano de nascimento da pessoa informando se ela é maior de idade ou menor.
+
+
+let idade = 2021
+let ano = 1992
+let resultado = idade - ano
+
+if (resultado >= 18) {
+    console.log(" É maior de idade!")
+}
+
+else {
+    console.log(" Menor de idade!")
+}
